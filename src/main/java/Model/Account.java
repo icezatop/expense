6b84d7e01c792bf40e7e942ceddd47bef2aa65a7
kawin ruangrit <icezatop@gmail.com>
2018-12-26 10:47:08 +0700

    Initial commit

---
+++ src/main/java/Model/Account.java
@@ -0,0 +1,60 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Account {
+    private double balance;
+    private static int id;
+    private ArrayList<String> explain;
+    private ArrayList<Double> history;
+
+    public Account(int money) {
+        balance = money;
+        id = 0;
+        explain = new ArrayList<>();
+        history = new ArrayList<>();
+    }
+    public Account() {
+        this.balance = 0;
+        id=0;
+        explain = new ArrayList<>();
+        history = new ArrayList<>();
+    }
+
+    public void income(double money,String explain){
+        id++;
+        this.balance+=money;
+        this.history.add(money);
+        this.explain.add(explain);
+    }
+
+    public void expense(double money,String explain){
+        id++;
+        this.balance -=money;
+        this.history.add(money);
+        this.explain.add(explain);
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+    public String getBalanceAndHistoty(){
+        String tmp = "";
+        for (int j = 0; j < history.size(); j++) {
+            tmp += "id :"+j+" " + history.get(j) + " " + explain.get(j) +"\n";
+        }
+        return tmp;
+    }
+//    public void editIncome(double money,String exp){
+//        if (money > history.get(id)){
+//
+//        }
+//
+//    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
+
+}
